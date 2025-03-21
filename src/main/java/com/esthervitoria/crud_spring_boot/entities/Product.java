e04517efdf2c5af59df6a46faf927b2dbd94271f
esther-vitoria <esther.vitoria21@outlook.com>
2025-03-21 09:08:39 -0300

    created entities classes

---
+++ src/main/java/com/esthervitoria/crud_spring_boot/entities/Product.java
@@ -0,0 +1,36 @@
+package com.esthervitoria.crud_spring_boot.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+import com.esthervitoria.crud_spring_boot.enumss.Category;
+
+@Entity
+@Table(name="products")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductVariation> productVariations;
+
+    private Boolean available;
+
+}
---
+++ src/main/java/com/esthervitoria/crud_spring_boot/enumss/Category.java
@@ -0,0 +1,8 @@
+package com.esthervitoria.crud_spring_boot.enumss;
+
+public enum Category {
+
+    PIZZA,
+    HAMBURGUER
+
+}
